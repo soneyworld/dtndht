2aec12841ab0347a26d75f0f5cf00378bd718153
Till Lorentzen <t.lorentzen@tu-bs.de>
2011-12-13 12:25:32 +0100

    renamed src dir to dtndht and also renamed lib

--- dtndht/dtndht.h
+++ dtndht/dtndht.h
@@ -1,4 +1,9 @@
 
+#ifndef DTNDHT_INCLUDED
+#define DTNDHT_INCLUDED
+#include <dtndht/dht.h>
+
+
 enum dtn_dht_event{
 	NONE=0,VALUES=1,VALUES6=2,SEARCH_DONE=3,SEARCH_DONE6=4
 };
@@ -60,3 +65,4 @@ int dtn_dht_periodic(const void *buf, size_t buflen,
 
 //int dtn_dht_insert_node(const unsigned char *id, struct sockaddr *sa, int salen);
 //int dtn_dht_ping_node(struct sockaddr *sa, int salen);
+#endif
