e1a8d07c7ae46959a6e1a75566736266fd4de750
Till Lorentzen <t.lorentzen@tu-bs.de>
2011-12-28 17:57:49 +0100

    Changed examination of the port from values

--- dtndht/dtndht.c
+++ dtndht/dtndht.c
@@ -8,9 +8,11 @@
 #include <arpa/inet.h>
 #include <openssl/rand.h>
 #include <netdb.h>
+#include <stdint.h>
 #include "dht.h"
 #include "dtndht.h"
 #include <openssl/sha.h>
+#include <netinet/in.h>
 
 #ifndef LOOKUP_THRESHOLD
 #define LOOKUP_THRESHOLD 1800
@@ -608,11 +610,16 @@ void dtn_dht_build_id_from_str(unsigned char *target, const char *s, size_t len)
 int cpyvaluetosocketstorage(struct sockaddr_storage *target, const void *value,
 		int type) {
 	if (type == AF_INET) {
-		memcpy(&((struct sockaddr_in*) target)->sin_addr, value, 4);
-		memcpy(&((struct sockaddr_in*) target)->sin_port, value + 4, 2);
+		struct sockaddr_in* sa_in = (struct sockaddr_in*)target;
+		sa_in->sin_port = 0;
+		memcpy(&(sa_in->sin_addr), value, 4);
+		memcpy(&(sa_in->sin_port), value + 4, 2);
+		sa_in->sin_port = ntohs(sa_in->sin_port);
 	} else if (type == AF_INET6) {
-		memcpy(&((struct sockaddr_in6*) target)->sin6_addr, value, 16);
-		memcpy(&((struct sockaddr_in6*) target)->sin6_port, value + 16, 2);
+		struct sockaddr_in6* sa_in = (struct sockaddr_in6*)target;
+		memcpy(&(sa_in->sin6_addr), value, 16);
+		memcpy(&(sa_in->sin6_port), value + 16, 2);
+		sa_in->sin6_port = ntohs(sa_in->sin6_port);
 	}
 	return 0;
 }
