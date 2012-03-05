668a7f613055e4f08360d4e887bf0396fe39432a
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-03-06 00:55:13 +0100

    Dokumentation added

--- dtndht/utils.h
+++ dtndht/utils.h
@@ -3,12 +3,17 @@
  *
  *  Created on: 08.02.2012
  *      Author: Till Lorentzen
+ *
  */
 
 #ifndef UTILS_H_
 #define UTILS_H_
 #include <sys/socket.h>
-
+/**
+ * This function copies the given value returned from the dht into the target sockaddr_storage
+ * This is very helpful, because all normal socket functions can handle the sockaddr_storage,
+ * but not the format, in which the dht answers
+ */
 int cpyvaluetosocketstorage(struct sockaddr_storage *target, const void *value,
 		int type);
 
