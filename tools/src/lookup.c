6a76f3a88a97c8db8794670c8751d9fb8e4c454a
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-03-18 17:40:44 +0100

    Added port and ipv4/ipv6 switches

--- tools/src/lookup.c
+++ tools/src/lookup.c
@@ -16,14 +16,24 @@
 #define MAX( a, b ) ( ((a) > (b)) ? (a) : (b) )
 #endif
 
-static volatile sig_atomic_t exiting = 0;
+static volatile sig_atomic_t exiting = 0, relookup = 0;
 static void sigexit(int signo) {
 	exiting = 1;
+}
+
+static void sigrelookup(int signo) {
+	relookup = 1;
 }
 
 static void init_signal(void) {
 	struct sigaction sa;
 	sigset_t ss;
+
+	sigemptyset(&ss);
+	sa.sa_handler = sigrelookup;
+	sa.sa_mask = ss;
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
 
 	sigemptyset(&ss);
 	sa.sa_handler = sigexit;
@@ -33,6 +43,7 @@ static void init_signal(void) {
 }
 
 static int _bootstrapped = 0;
+static int _shutdown_after_successful_lookup = 1;
 static struct dtn_dht_context _context;
 void bootstrapping() {
 	if (_bootstrapped > time(NULL) + 30) {
@@ -43,7 +54,8 @@ void bootstrapping() {
 }
 
 void dtn_dht_operation_done(const unsigned char *info_hash) {
-	exiting = 1;
+	if (_shutdown_after_successful_lookup)
+		exiting = 1;
 	return;
 }
 
@@ -105,8 +117,42 @@ void dtn_dht_handle_lookup_result(const struct dtn_dht_lookup_result *result) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		printf("usage: dhtlookup <eid>\n\n");
+	int c, ipv4 = 0, ipv6 = 0;
+	int port = 9999;
+	opterr = 0;
+	char *lookupstr = NULL;
+	while ((c = getopt(argc, argv, "46p:l:")) != -1) {
+		switch (c) {
+		case 'l':
+			lookupstr = optarg;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			if (port < 1) {
+				fprintf(stderr, "Illegal port number %s\n", optarg);
+				return -1;
+			}
+			break;
+		case '4':
+			ipv4 = 1;
+			break;
+		case '6':
+			ipv6 = 1;
+			break;
+		case '?':
+			if (optopt == 'l' || optopt == 'p')
+				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+			else if (isprint(optopt))
+				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+			return 1;
+		default:
+			return -1;
+		}
+	}
+	if (!lookupstr) {
+		fprintf(stderr, "Usage: dhtlookup [options] -l <eid>\n\n");
 		return -1;
 	}
 	int rc = 0;
@@ -114,6 +160,12 @@ int main(int argc, char *argv[]) {
 	int lookup_done = 0;
 	unsigned char _buf[4096];
 	dtn_dht_initstruct(&_context);
+	_context.port = port;
+	if (ipv4 && !ipv6) {
+		_context.type = IPV4ONLY;
+	} else if (!ipv4 && ipv6) {
+		_context.type = IPV6ONLY;
+	}
 	if (dtn_dht_init_sockets(&_context) != 0) {
 		dtn_dht_close_sockets(&_context);
 		return -1;
@@ -133,17 +185,20 @@ int main(int argc, char *argv[]) {
 			bootstrapping();
 		}
 		ready = dtn_dht_ready_for_work(&_context);
-		if (ready && !lookup_done) {
-			dtn_dht_lookup(&_context, argv[1], strlen(argv[1]));
+		if ((ready && !lookup_done) || relookup) {
+			dtn_dht_lookup(&_context, lookupstr, strlen(lookupstr));
 			lookup_done = 1;
+			relookup = 0;
 		}
 		struct timeval tv;
 		fd_set readfds;
 		tv.tv_sec = tosleep;
 		tv.tv_usec = random() % 1000000;
 		FD_ZERO(&readfds);
-		FD_SET(_context.ipv4socket, &readfds);
-		FD_SET(_context.ipv6socket, &readfds);
+		if (_context.ipv4socket >= 0)
+			FD_SET(_context.ipv4socket, &readfds);
+		if (_context.ipv6socket >= 0)
+			FD_SET(_context.ipv6socket, &readfds);
 		rc = select(high_fd + 1, &readfds, NULL, NULL, &tv);
 		if (exiting)
 			break;
