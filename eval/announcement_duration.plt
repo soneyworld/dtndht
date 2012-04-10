7e12428b14ad40132c40d62d9a05ba22a0245ae4
Till Lorentzen <t.lorentzen@tu-bs.de>
2012-04-10 11:01:19 +0200

    Changed the announcement title and spam summary order

--- eval/announcement_duration.plt
+++ eval/announcement_duration.plt
@@ -1,7 +1,15 @@
-set terminal png truecolor font small size 1000,1000
+set terminal png truecolor font "Helvetica" 10 size 1000,800
 set output 'announcement_duration.png'
 set datafile separator ";"
-plot 'announcement_duration.csv' every::1::500 using 3:4 with lines
-set terminal png truecolor font small size 1000,1000
+set ylabel 'duration until start [sec]'
+set xlabel 'timeline [hours]'
+set xtics 3600
+set ytics 0,60
+set yrange [0:840]
+set format x ""
+set title "Announcement Duration" font "Helvetica,12"
+plot 'announcement_duration.csv' every::1::500 using 3:4 with lines title "2012-03-28"
+set terminal png truecolor font "Helvetica" 10 size 1000,800
 set output 'announcement_duration2.png'
-plot 'announcement_duration.csv' every::501::1001 using 3:4 with lines
+set title "Announcement Duration" font "Helvetica,12"
+plot 'announcement_duration.csv' every::501::1001 using 3:4 with lines title "2012-04-01"
--- eval/random_lookup.pl
+++ eval/random_lookup.pl
@@ -8,9 +8,57 @@ if ($num_args < 1) {
   exit;
 }
 
+my $PRINTALL = 0;
+my %CountryDirections;
+my %OutgoingDirections;
+my %IncommingDirections;
+my @ValuesPerLookup;
+my @AnswerPerLookup;
+my $lookupCounter = 0;
+my $summaryoutputfile = "random_lookup_summary.csv";
+my $summaryoutputfile_outgoing = "random_lookup_summary_out.csv";
+my $summaryoutputfile_incomming = "random_lookup_summary_in.csv";
 foreach (@ARGV) {
 	parseFile($_);
 }
+
+open(OUT, ">$summaryoutputfile");
+print OUT "Direction;Count;\n";
+foreach ( sort {$CountryDirections{$b} <=> $CountryDirections{$a}} keys %CountryDirections ) {
+	my $direction = $_;
+	my $count = $CountryDirections{"$direction"};
+	print OUT "$direction;$count;\n";
+}
+close(OUT);
+open(OUT, ">$summaryoutputfile_outgoing");
+print OUT "Source;OutgoingCount;\n";
+foreach ( sort {$OutgoingDirections{$b} <=> $OutgoingDirections{$a}} keys %OutgoingDirections ) {
+	my $source = $_;
+	my $count = $OutgoingDirections{"$source"};
+	print OUT "$source;$count;\n";
+}
+close(OUT);
+open(OUT, ">$summaryoutputfile_incomming");
+print OUT "Destination;IncommingCount;\n";
+foreach ( sort {$IncommingDirections{$b} <=> $IncommingDirections{$a}} keys %IncommingDirections ) {
+	my $destination = $_;
+	my $count = $IncommingDirections{"$destination"};
+	print OUT "$destination;$count;\n";
+}
+close(OUT);
+
+my $answerCounter = 0;
+my $valueCounter = 0;
+foreach ( @AnswerPerLookup ) {
+	$answerCounter += $_;
+}
+foreach ( @ValuesPerLookup ) {
+	$valueCounter += $_;
+}
+print "Number of lookups: $lookupCounter\n";
+print "Number of answers: $answerCounter\n";
+print "Number of values : $valueCounter\n";
+exit;
 
 sub parseFile {
 my $inputfile = $_[0];
@@ -52,6 +100,7 @@ while ($line)
 			}
 			# Speicher den Hash und die EID ab:
 			$eids{"$sha1"} = "$eid";
+			$lookupCounter ++;
 		}
 		if ($line =~ m/DHT-EVALUATION VALUES_FOUND_EVENT HASH=/) {
 			my @tokens = split(/\s+/, $line);
@@ -59,6 +108,7 @@ while ($line)
 			my $eid;
 			my $ip;
 			my $port;
+			my $sourceCountry;
 			foreach (@tokens) {
 				if( $_ =~ m/^HASH=/) {
 					my @subtokens = split(/=/, $_);
@@ -87,10 +137,29 @@ while ($line)
 			}
 			if( $ip ) {
 				push(@answering_ips,"$ip". "_" ."$port");
+				$sourceCountry = geoip("$ip");
 			}
 			$line = <IN>;
 			while ( $line =~m/^---->/ ) {
 				@tokens = split(/\s+/, $line);
+				my $destinationCountry = geoip($tokens[1]);
+				if (exists $CountryDirections{"$sourceCountry to $destinationCountry"}) {
+					$CountryDirections{"$sourceCountry to $destinationCountry"}++;
+				} else {
+					$CountryDirections{"$sourceCountry to $destinationCountry"} = 1;
+				}
+				if (not $destinationCountry eq $sourceCountry ) {
+					if(exists $OutgoingDirections{"$sourceCountry"}){
+						$OutgoingDirections{"$sourceCountry"} ++;
+					} else {
+						$OutgoingDirections{"$sourceCountry"} = 1;
+					}
+					if(exists $IncommingDirections{"$destinationCountry"}){
+						$IncommingDirections{"$destinationCountry"} ++;
+					} else {
+						$IncommingDirections{"$destinationCountry"} = 1;
+					}
+				}
 				push(@value_ips, "$tokens[1]"."_"."$tokens[2]");
 				$line = <IN>;
 			}
@@ -101,16 +170,23 @@ while ($line)
 }
 
 close(IN);
+if ($PRINTALL) {
 open(OUT,">$outputfile");
-
 print OUT "EID;NumberOfAnsweringNodes;NumberOfPossibleTargets\n";
+}
 foreach ( values %eids ) {
 	my $eid = $_;
 	my $count = $answers{"$eid"};
 	my $value_count = $values{"$eid"};
-	print OUT "$eid;$count;$value_count\n";
+	push(@ValuesPerLookup,"$value_count");
+	push(@AnswerPerLookup,"$count");
+	if ($PRINTALL) {
+		print OUT "$eid;$count;$value_count\n";
+	}
 }
-
+if ( not $PRINTALL) {
+	return;
+}
 print OUT "\n---------------------------------------\n\n";
 print OUT "AnsweringIP;Port;Country\n";
 foreach (@answering_ips) {
